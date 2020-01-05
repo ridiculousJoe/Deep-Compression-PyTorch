c3870184711945ae17e89a36c2b60b6907b343de
ridiculousJoe <erzhuo1026@gmail.com>
2020-01-05 06:29:01 +0000

    add small-lenet.py

--- util.py
+++ util.py
@@ -8,7 +8,7 @@ import torch.nn.functional as F
 from torchvision import datasets, transforms
 
 def log(filename, content):
-    with open(filename, 'w') as f:
+    with open(filename, 'a') as f:
         content += "\n"
         f.write(content)
 
@@ -33,7 +33,9 @@ def print_nonzeros(model):
         total_params = np.prod(tensor.shape)
         nonzero += nz_count
         total += total_params
+        print(f'{name:20} | nonzeros = {nz_count:7} / {total_params:7} ({100 * nz_count / total_params:6.2f}%) | total_pruned = {total_params - nz_count :7} | shape = {tensor.shape}')
         output += f'{name:20} | nonzeros = {nz_count:7} / {total_params:7} ({100 * nz_count / total_params:6.2f}%) | total_pruned = {total_params - nz_count :7} | shape = {tensor.shape}\n'
+    print(f'alive: {nonzero}, pruned : {total - nonzero}, total: {total}, Compression rate : {total/nonzero:10.2f}x  ({100 * (total-nonzero) / total:6.2f}% pruned)')
     output += f'alive: {nonzero}, pruned : {total - nonzero}, total: {total}, Compression rate : {total/nonzero:10.2f}x  ({100 * (total-nonzero) / total:6.2f}% pruned)\n'
     return output
 
